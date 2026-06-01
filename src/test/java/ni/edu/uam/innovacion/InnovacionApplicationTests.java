b221fdb3c269ada3e690a2c5fccb9f7bffd8e07c
SteMt323 <stevenmt@uamv.edu.ni>
2026-05-31 22:37:08 -0600

    feat: subiendo estructura del repo

---
+++ src/test/java/ni/edu/uam/innovacion/InnovacionApplicationTests.java
@@ -0,0 +1,13 @@
+package ni.edu.uam.innovacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InnovacionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
